b69230a36e7b3a70ee47a3a171bd439746609f44
Stephen <stephenn649@gmail.com>
2025-01-30 13:11:47 -0600

    Added Contracts Screen component

---
+++ components/Contracts/ContractsScreen.js
@@ -0,0 +1,20 @@
+import { Text, View, StyleSheet } from "react-native";
+
+
+const ContractsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Contracts go here</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default ContractsScreen;
