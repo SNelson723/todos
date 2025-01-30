04fa2ea007f516f102303d5e55ab1e37a58cf6e0
Stephen <stephenn649@gmail.com>
2025-01-30 13:31:25 -0600

    Added contractSlice and am getting the contracts from the API in ContractsScreen.js

--- components/Contracts/ContractsScreen.js
+++ components/Contracts/ContractsScreen.js
@@ -1,7 +1,23 @@
-import { Text, View, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { getData } from "../../src/api/getData";
+import { useToken } from "../../src/token";
 
+const ContractsScreen = ({ route }) => {
+  const token = useToken();
 
-const ContractsScreen = () => {
+  React.useEffect(() => {
+    const fetchContracts = async () => {
+      const contracts = await getData(
+        "https://devapi.dcr-support.com/mobile/DCRStores/Contracts",
+        token
+      );
+      console.log(contracts.length)
+    };
+
+    fetchContracts();
+  }, [token]);
+
   return (
     <View style={styles.container}>
       <Text>Contracts go here</Text>
